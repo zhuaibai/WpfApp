237d50826a8f0a4cdced6e67342cd3f3cc649c00
罗福全 <778393374@qq.com>
2025-04-12 17:27:11 +0800

    搞了半天Placeholder功能，没有实现

---
+++ WpfApp1/Behavior/WatermarkTextBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1.Behavior
+{
+    public class WatermarkTextBox:TextBox
+    {
+
+        //水印
+        public string Watermark
+        {
+            get { return (string)GetValue(WatermarkProperty); }
+            set { SetValue(WatermarkProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Watermark.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WatermarkProperty =
+            DependencyProperty.Register("Watermark", typeof(string), typeof(WatermarkTextBox));
+
+
+    }
+}
